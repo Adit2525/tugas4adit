fb393ad21d2f90b5d370a7c38a55bfbf22c7070f
Adit2525 <160556720+Adit2525@users.noreply.github.com>
2024-09-12 10:46:35 +0700

    Create mainActivity.kt

---
+++ mainActivity.kt
@@ -0,0 +1,45 @@
+package com.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameEditText = findViewById(R.id.username_input)
+        passwordEditText = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_btn)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "123456") {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("EXTRA_MESSAGE", "Hello from MainActivity")
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(
+                    this,
+                    "username atau password salah",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    }}
